5cb1ebcded7404a67dba9ca4045a0b89fb203614
Surajit Sadhukhan <surajit1992@live.com>
2023-07-05 14:54:42 +0530

    all

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,13 @@ ls
 echo "Surajit"
 
 # echo "Deploying to ${DEPLOY_SERVER}"
-scp -r public/ administrator:${PASSWORD}@91.107.229.125:/C:/Users/Administrator/Desktop/build
+# scp -r public/ administrator:${PASSWORD}@91.107.229.125:/C:/Users/Administrator/Desktop/build
 # scp -r public/ surajit:surajit@91.107.229.125:build
 # cp -r public/ ftp://surajit:surajit@91.107.229.125:/build
+
+password="Surajit@1992"
+username="administrator"
+Ip="91.107.229.125"
+sshpass -p "$password" scp public/ $username@$Ip:/C:/Users/Administrator/Desktop/build
 
 echo "Finished copying the build files"
