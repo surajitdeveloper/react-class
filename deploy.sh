a2add47b9fecfb1e6315536695fad0fb5abf5155
Surajit Sadhukhan <surajit1992@live.com>
2023-07-05 14:21:24 +0530

    all

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,10 @@ echo "Surajit"
 
 # echo "Deploying to ${DEPLOY_SERVER}"
 # scp -r public/ surajit:surajit@91.107.229.125:build
-cp -r public/ surajit:surajit@91.107.229.125:/build
-
+# cp -r public/ ftp://surajit:surajit@91.107.229.125:/build
+ftp 91.107.229.125
+surajit
+surajit
+put public
+bye
 echo "Finished copying the build files"
