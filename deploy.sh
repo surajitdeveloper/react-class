1c9a2ea1a74a11dcbcd90a181c090a90f11620e7
Surajit Sadhukhan <surajit1992@live.com>
2023-07-05 14:45:24 +0530

    all

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 # DEPLOY_SERVER=$DEPLOY_SERVER
 # SERVER_FOLDER="html-folder-in-server"
+PASSWORD="Surajit@1992"
 
 # Building React output
 # npm install
@@ -14,11 +15,8 @@ ls
 echo "Surajit"
 
 # echo "Deploying to ${DEPLOY_SERVER}"
+scp -r public/ administrator:${PASSWORD}@91.107.229.125:/C:/Users/Administrator/Desktop/build
 # scp -r public/ surajit:surajit@91.107.229.125:build
 # cp -r public/ ftp://surajit:surajit@91.107.229.125:/build
-ftp 91.107.229.125
-surajit
-surajit
-put public
-bye
+
 echo "Finished copying the build files"
